9d32876d8e69d0db3201170361e3d4b2d3bbf9af
XYROC <xiroc.minecraft@gmail.com>
2019-11-16 12:10:23 +0100

    added server side compatibility

--- Beneficium 1.12.2/src/main/java/xiroc/beneficium/common/util/Reference.java
+++ Beneficium 1.12.2/src/main/java/xiroc/beneficium/common/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "beneficium";
 	public static final String NAME = "Beneficium";
-	public static final String VERSION = "0.5.0";
+	public static final String VERSION = "0.5.1";
 	public static final String VERSION2 = VERSION;
 	public static final String MC_VERSIONS = "[1.12.2]";
 	public static final String DEPENDENCIES = "required:baubles";
